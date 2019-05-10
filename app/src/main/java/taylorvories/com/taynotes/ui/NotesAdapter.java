54ea2893e650cec8971052ed97f7d3361e0ab445
Taylor Vories <taylor@tmtech.me>
2019-05-09 20:42:10 -0600

    So much stuff

---
+++ app/src/main/java/taylorvories/com/taynotes/ui/NotesAdapter.java
@@ -0,0 +1,52 @@
+package taylorvories.com.taynotes.ui;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import taylorvories.com.taynotes.R;
+import taylorvories.com.taynotes.model.NoteEntity;
+
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder> {
+
+    private final List<NoteEntity> mNotes;
+    private final Context mContext;
+
+    public NotesAdapter(List<NoteEntity> mNotes, Context mContext) {
+        this.mNotes = mNotes;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.note_text)
+        TextView mTextView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
