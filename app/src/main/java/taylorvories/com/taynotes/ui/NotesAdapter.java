080c7f23b577624dbccf3416d012ee073bb0e310
Taylor Vories <taylor@tmtech.me>
2019-05-09 20:48:39 -0600

    Recyclerview added and working

--- app/src/main/java/taylorvories/com/taynotes/ui/NotesAdapter.java
+++ app/src/main/java/taylorvories/com/taynotes/ui/NotesAdapter.java
@@ -3,6 +3,7 @@ package taylorvories.com.taynotes.ui;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -26,18 +27,21 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder> 
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.note_list_item, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final NoteEntity note = mNotes.get(position);
+        holder.mTextView.setText(note.getText());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mNotes.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
